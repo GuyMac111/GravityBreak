aa988232c297ef65b192a295ce797206540f9fb4
gmclean <gmclean@gsngames.com>
2018-09-13 23:37:22 +0100

    [Module-loading] Added additional comment explaining the use of requirejs.config for future ref

--- bin/js/main.js
+++ bin/js/main.js
@@ -1,6 +1,9 @@
 requirejs.config({
     baseUrl: './js',
     waitSeconds: 20,
+    //This is like saying "stuff in this window module is dependent upon the content of deps, 
+    //the definitions of which can be found at the baseUrl. And then "Unless specified in 'paths'. 
+    //In which case require will look there for any exports matching that name.
     deps: ["typescript-collections"],
     paths:{
         "typescript-collections":"../../node_modules/typescript-collections/dist/lib/umd"
