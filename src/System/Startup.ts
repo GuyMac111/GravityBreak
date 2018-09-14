a7bb289b061f838a38f6119f02e64359fda0e395
gmclean <gmclean@gsngames.com>
2018-09-14 23:27:13 +0100

    [Startup] Added some things meant to substitute in for some vague kind of 'bootstrap' and 'context'

---
+++ src/System/Startup.ts
@@ -0,0 +1,47 @@
+import { BlockFactory } from "../Block/BlockFactory";
+import { SystemModel } from "./SystemModel";
+import { ISystemModel } from "./ISystemModel";
+import { GridController } from "../Grid/GridController";
+
+export class Startup{
+    _game: Phaser.Game;
+    ////
+    //hmmmmm: Does this need to exist?
+    _systemModel: SystemModel;
+    ////
+
+    ///Perhaps we should separate Startup into 1: Bootstrap and 2: Initialise
+    ///Don't really intend to create a full context so we can keep things close in here for the time being
+    ///and then decide to split things apart if things get too tightly coupled
+    
+    constructor(game: Phaser.Game){
+        this._game = game;
+    }
+    
+    initialiseGame(){
+        this.bootstrapGame();
+        this.initialiseGrid();
+    }
+
+    get systemModel(): ISystemModel{
+        return this._systemModel;
+    }
+
+    private bootstrapGame(){
+        this.bootstrapBlockFactory();
+    }
+
+    private initialiseGrid(){
+        let gridController: GridController = new GridController(10,10);
+    }
+
+    private bootstrapBlockFactory(){
+        let blockLayerGroup: Phaser.Group = this._game.add.group();
+        let blockFactory: BlockFactory = new BlockFactory(this._game, blockLayerGroup)
+        this._systemModel.setBlockFactory(blockFactory);
+    }
+
+
+
+
+}
